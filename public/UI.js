7c2f268c358b1b9a38ef80faecfa641ab9ac0a0f
Michael Adair <mjadair91@gmail.com>
2020-06-01 16:37:35 +0100

    updated UI interactions

--- public/UI.js
+++ public/UI.js
@@ -1,5 +1,6 @@
 const wHeight = $(window).height()
 const wWidth = $(window).width()
+let player = {}
 
 const canvas = document.querySelector('#the-canvas')
 const context = canvas.getContext('2d')
@@ -15,5 +16,16 @@ $('#loginModal').modal('show')
 $('.name-form').submit((event) => {
   event.preventDefault()
   // console.log('submitted!')
+  player.name = document.querySelector('#name-input').value
+  console.log(player)
+  $('#loginModal').modal('hide')
+  $('#spawnModal').modal('show')
+  document.querySelector('.player-name').innerHTML = player.name
+})
 
+
+$('.start-game').click((event) => {
+  $('.modal').modal('hide')
+  $('.hiddenOnStart').removeAttr('hidden')
+  init()
 })
