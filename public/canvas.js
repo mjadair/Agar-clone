cd5a238b6afd5ff28f6d11f6eae2c12cfaed4d6f
Michael Adair <mjadair91@gmail.com>
2020-06-02 10:11:17 +0100

    assigned random x and y co-ordinates

---
+++ public/canvas.js
@@ -0,0 +1,19 @@
+function init() {
+  draw()
+}
+
+
+
+
+const randomX = Math.floor(500 * Math.random() + 10)
+const randomY = Math.floor(500 * Math.random() + 10)
+
+console.log(randomX, randomY)
+
+function draw() {
+
+
+
+
+
+}
