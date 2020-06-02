cd805b80676cdd875881cc7498fa25d82c759940
Michael Adair <mjadair91@gmail.com>
2020-06-02 10:20:54 +0100

    drew a cicle randomly with random position on the canvas

--- public/canvas.js
+++ public/canvas.js
@@ -13,6 +13,20 @@ console.log(randomX, randomY)
 function draw() {
 
 
+  context.beginPath()
+  context.fillStyle = 'rgb(255,0,0)'
+  
+  //arc takes the x and y co-ordinates of the centre of the arc
+  //the third argument is the radius of the circle
+  //the 4th aguments is the starting angle
+  //the 5th argument is where to stop (PI *2 will give a full circumference)
+  context.arc(randomX, randomY, 10, 0, Math.PI * 2)
+  context.fill()
+  context.lineWidth = 3
+  context.strokeStyle = 'rgb(0,255,0)'
+  context.stroke()
+  requestAnimationFrame(draw)
+
 
 
 
